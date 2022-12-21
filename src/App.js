5fa5836b7ade3ebaf9469a6d649128fbccc563aa
Joel Singh <joelsingh788@gmail.com>
2022-12-21 11:03:31 -0500

    Change app class name to be consistent with everything else

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import appCss from "./App.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
       <Counter
         className="current-score"
