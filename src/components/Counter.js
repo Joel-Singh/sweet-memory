310dc76750bb4a65e20cffec32508b87adbf9268
Joel Singh <joelsingh788@gmail.com>
2022-12-21 11:39:00 -0500

    Add counter class to Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,6 @@
 export default function Counter(props) {
   const { text, count, className } = props
   return (
-    <span className={className}>{text + " " + count}</span>
+    <span className={className + " counter"}>{text + " " + count}</span>
   )
 }
