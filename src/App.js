54a3a605e2d58b6f01a6f9d36752575338448f5f
Joel Singh <joelsingh788@gmail.com>
2022-12-21 12:30:47 -0500

    Have currentScore and bestScore utilize state for their counts

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import Header from "./components/Header";
 import Counter from "./components/Counter";
 import CardHolder from "./components/CardHolder";
 import appCss from "./App.css"
+import { useState } from "react";
 
 function App() {
+  const [currentScore, setCurrentScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
+
   return (
     <div className="app">
       <Header />
       <Counter
         className="current-score"
         text="Current:"
-        count="3"
+        count={currentScore}
       />
       <Counter
         className="best-score"
         text="Best:"
-        count="12"
+        count={bestScore}
       />
       <CardHolder shouldShuffle={true} />
     </div>
