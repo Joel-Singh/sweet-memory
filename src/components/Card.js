062aa914b7591809ca9e53cf6d30b1a584ea9393
Joel Singh <joelsingh788@gmail.com>
2022-12-22 16:30:49 -0500

    Add id component to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,11 @@
 export default function Card(props) {
-  const { image, onClick } = props
+  const { image, onClick, id } = props
   return (
     <img
       className="card"
       src={image}
       onClick={onClick}
+      id={id}
     />
   )
 }
