dfc757e13207cff7f278824431d13771b82ea194
Joel Singh <joelsingh788@gmail.com>
2022-12-23 13:01:38 -0500

    Make game functional, keeps track of current and best

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import Header from "./components/Header";
 import Counter from "./components/Counter";
 import CardHolder from "./components/CardHolder";
 import appCss from "./App.css"
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 let clickedCards = []
 function App() {
   const [currentScore, setCurrentScore] = useState(0)
+  const currentScoreRef = useRef()
+  currentScoreRef.current = currentScore
+
   const [bestScore, setBestScore] = useState(0)
+  const bestScoreRef = useRef()
+  bestScoreRef.current = bestScore
 
   const [cardHolderOnScreen, setCardHolderOnScreen] =
     useState(<CardHolder shouldShuffle={true} onCardClick={onCardClick}/>)
@@ -31,12 +36,30 @@ function App() {
 
   function onCardClick(event) {
     const clickedCard = event.target.id
+    const currentScore = currentScoreRef.current
+    const bestScore = bestScoreRef.current
+
+    if (!hasCardBeenClickedBefore(clickedCard)) {
+      setCurrentScore(prevScore => prevScore + 1)
+
+      if (currentScore >= bestScore) {
+        setBestScore(currentScore + 1)
+      }
+    } else if (hasCardBeenClickedBefore(clickedCard)) {
+      setCurrentScore(0)
+      clickedCards = []
+    }
+
     clickedCards.push(clickedCard)
     shuffleCardHolder()
   }
 
   function shuffleCardHolder() {
     setCardHolderOnScreen(<CardHolder shouldShuffle={true} onCardClick={onCardClick}/>)
+  }
+
+  function hasCardBeenClickedBefore(card) {
+    return clickedCards.includes(card)
   }
 }
 
